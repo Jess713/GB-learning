7d7316ba90a97acd1503cec6394abb833e3923b0
jas_w <jas_wong168@hotmail.com>
2019-10-31 00:23:53 -0700

    Revert "combined jason's youtube to repo"
    This reverts commit 211e79720e81347de31663c040960d613ac36b51.

--- project/screens/SettingsScreen.js
+++ project/screens/SettingsScreen.js
@@ -1,92 +1,13 @@
-// import React from 'react';
-// import { ExpoConfigView } from '@expo/samples';
+import React from 'react';
+import { ExpoConfigView } from '@expo/samples';
 
-// export default function SettingsScreen() {
-//   /**
-//    * Go ahead and delete ExpoConfigView and replace it with your content;
-//    * we just wanted to give you a quick view of your config.
-//    */
-//   return <ExpoConfigView />;
-// }
-
-// SettingsScreen.navigationOptions = {
-//   title: 'app.json',
-// };
-
-import React, { Component } from 'react';
-import { StyleSheet, SafeAreaView, FlatList, Text, TouchableOpacity } from 'react-native';
-import { Actions, Scene, Router } from 'react-native-router-flux';
-const MAX_RESULT = 15;
-const PLAYLIST_ID = "PLb2quItt3GbhQV9yvsMdszoTwQ-_7JaLA";
-const API_KEY = "AIzaSyCJtoZ4XuDc-m6Y6gIltSKj3RX9jigP2mM";
-export default class SettingsScreen extends Component<{}> {
-
-settingsscreen(){
-    Actions.settingsscreen();
-  }
-
-
-  watchVideo(video_url){
-    Actions.watchvideo({video_url: video_url});
-  }
-
-
-  componentWillMount() {
-    this.fetchPlaylistData();
-  }
-  fetchPlaylistData = async () => {
-    const response = await fetch(`https://www.googleapis.com/youtube/v3/playlistItems?playlistId=${PLAYLIST_ID}&maxResults=${MAX_RESULT}&part=snippet%2CcontentDetails&key=${API_KEY}`);
-    const json = await response.json();
-    this.setState({ videos: json['items']});
-    console.log(this.state.videos)
-  };
-  constructor(props) {
-    super(props);
-    this.state = {
-      videos: [],
-    }
-  }
-  render() {
-    const videos = this.state.videos;
-    return (
-      <SafeAreaView style={styles.safeArea}>
-            <FlatList
-              data={this.state.videos}
-              keyExtractor={(_, index) => index.toString()}
-              renderItem={
-                ({item}) => 
-                <TouchableOpacity
-                    style={styles.demacate}
-                    onPress={() => this.watchVideo(item.contentDetails.videoId)}
-                >
-                <Text 
-                  style={styles.item} 
-                > 
-                {item.snippet.title} 
-                </Text>
-                </TouchableOpacity>
-              }
-            />
-      </SafeAreaView>
-    );
-  }
+export default function SettingsScreen() {
+  /**
+   * Go ahead and delete ExpoConfigView and replace it with your content;
+   * we just wanted to give you a quick view of your config.
+   */
+  return <ExpoConfigView />;
 }
-const styles = StyleSheet.create({
-  safeArea: {
-    flex: 1,
-    backgroundColor: '#fff'
-  },
-  demacate: {
-    borderBottomColor: 'blue',
-    borderBottomWidth: 2,
-    borderRadius:10
-  },
-  item: {
-    padding: 10,
-    fontSize: 12,
-    height: 44,
-  },
-});
 
 SettingsScreen.navigationOptions = {
   title: 'app.json',
