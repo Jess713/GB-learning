92b57d6d51637197086ab0822f7b76aefbf8379e
A01057802 <henrykang720@Gmail.com>
2019-10-30 23:24:06 -0700

    created Sign up pge

--- project/screens/LinksScreen.js
+++ project/screens/LinksScreen.js
@@ -1,16 +1,26 @@
 import React from 'react';
-import { ScrollView, StyleSheet } from 'react-native';
+import { ScrollView, StyleSheet, Text, View, TextInput, TouchableOpacity } from 'react-native';
 import { ExpoLinksView } from '@expo/samples';
 
 export default function LinksScreen() {
   return (
-    <ScrollView style={styles.container}>
-      {/**
-       * Go ahead and delete ExpoLinksView and replace it with your content;
-       * we just wanted to provide you with some helpful links.
-       */}
-      <ExpoLinksView />
-    </ScrollView>
+    <View style={styles.container}>
+      <View style={styles.innerView}>
+      <Text style={styles.header}>Registration</Text>
+
+      <TextInput style={styles.textinput}placeholder="Your name" underlineColorAndroid={'transparent'}/>
+
+      <TextInput style={styles.textinput}placeholder="Your Email" underlineColorAndroid={'transparent'}/>
+      <TextInput style={styles.textinput}placeholder="Your Password" underlineColorAndroid={'transparent'}/>
+
+      <TouchableOpacity style={styles.button}>
+        <Text style={styles.btntext}>Sign up</Text>
+      </TouchableOpacity>
+      </View>
+      
+
+
+    </View>
   );
 }
 
@@ -20,8 +30,49 @@ LinksScreen.navigationOptions = {
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    paddingTop: 15,
-    backgroundColor: '#fff',
+    alignSelf: 'stretch',
+    justifyContent:'center',
+    alignContent:'center',
+    textAlign:'center',
+    alignItems:'center',
+    
   },
+  innerView:{
+    alignItems:'center',
+    width:400,
+    height:500,
+    borderWidth:5,
+    borderColor:'rgba(128,128,128,0.4)'
+  },
+  header:{
+    fontSize: 24,
+    color: '#fff',
+    paddingBottom: 10,
+    marginBottom:40,
+    borderBottomColor:'#199187',
+    borderBottomWidth: 1,
+    color:'rgb(0,0,0)',
+    fontWeight:'bold',
+  },
+  textinput:{
+    alignSelf:'stretch',
+    //weight:50,
+    height:40,
+    marginBottom:30,
+    color: '#fff',
+    borderBottomColor:'#f8f8f8',
+    borderBottomWidth:1,
+    textAlign: "center", 
+  },
+  button:{
+    alignSelf:'stretch',
+    alignItems:'center',
+    padding:20,
+    backgroundColor: '#59cbbd',
+    marginTop: 30,
+  },
+  btntext:{
+    color:'#fff',
+    fontWeight:'bold',
+  }
 });
