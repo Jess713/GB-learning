ce1dcef673cfb25516dcbe1458d099a1fe2a8ff6
A01057802 <henrykang720@Gmail.com>
2019-11-01 19:18:12 -0700

    sh: wq: command not found

--- project/screens/HomeScreen.js
+++ project/screens/HomeScreen.js
@@ -43,10 +43,10 @@ export default class App extends Component {
       
       <Background>
       <Logo />
-      <Header>Firebase Login</Header>
+      <Header>Granville Biomedical Inc.</Header>
   
       <Paragraph>
-        This template supports Firebase authorization out of the box.
+        
       </Paragraph>
       <Button mode="contained" onPress={() => navigation.navigate("LoginScreen")}>
         Login
@@ -63,28 +63,6 @@ export default class App extends Component {
   }
 }
 
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
-    backgroundColor: '#ecf0f1',
-  },
-  input: {
-    width: 200,
-    height: 44,
-    padding: 10,
-    borderWidth: 1,
-    borderColor: 'black',
-    marginBottom: 10,
-  },
-  image: {
-    width:100,
-    height:100,
-    display:'flex',
-  }
-});
 
 
 function handleLearnMorePress() {
