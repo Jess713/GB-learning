1ec2d013c978d36c8af289472c984d53847608ac
Henry <henrykang720@gmail.com>
2019-11-08 21:14:18 -0800

    UI fixed

--- project/components/Background.js
+++ project/components/Background.js
@@ -20,13 +20,15 @@ const Background = ({ children }) => (
 const styles = StyleSheet.create({
   background: {
     flex: 1,
-    width: "100%"
+    width: "100%",
+    backgroundColor:"#8385ad",
   },
   container: {
+    backgroundColor:"#b8b8d2",
     flex: 1,
     padding: 20,
     width: "100%",
-    maxWidth: 350,
+    maxWidth: 380,
     alignSelf: "center",
     alignItems: "center",
     justifyContent: "center"
--- project/components/Header.js
+++ project/components/Header.js
@@ -2,11 +2,14 @@ import React, { memo } from "react";
 import { StyleSheet, Text } from "react-native";
 import { theme } from "../core/theme";
 
-const Header = ({ children }) => <Text style={styles.header}>{children}</Text>;
+const Header = ({ children }) =>
+ <Text style={styles.header}>{children}</Text>;
 
 const styles = StyleSheet.create({
   header: {
     fontSize: 26,
+    //fontFamily:'Jomolhari',
+    fontFamily:'Arial',
     color: "rgb(147,143,180)",
     fontWeight: "bold",
     paddingVertical: 14,
